5d7115b67cc7e35920aac80d2db5d2ea8c3b80b2
Signior-X <b19188@students.iitmandi.ac.in>
2022-03-21 12:40:25 +0530

    Create generate random class file

---
+++ GenerateRandom.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class GenerateRandom {
+
+    public void printArrayList(ArrayList<Integer> arl) {
+        for (int i = 0; i < arl.size(); i++) {
+            System.out.print(arl.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public int generateRandom() {
+        return (int) (Math.random() * 10);
+    }
+
+    public ArrayList<Integer> generateRandomArrayList(int n) {
+        System.out.println("Generating random numbers for n = " + n);
+
+        ArrayList<Integer> arl = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            arl.add(generateRandom());
+        }
+
+        return arl;
+    }
+
+    public static void main(String args[]) {
+        GenerateRandom obj = new GenerateRandom();
+        obj.printArrayList(obj.generateRandomArrayList(1000));
+    }
+}
---
+++ Q1_Stream.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Q1_Stream {
+
+    public static void main(String args[]) {
+
+        int n = 1000;
+        // we will take n from the args
+        if (args.length > 0) {
+            n = Integer.parseInt(args[0]);
+        }
+
+        GenerateRandom obj = new GenerateRandom();
+        obj.printArrayList(obj.generateRandomArrayList(n));
+    }
+}
